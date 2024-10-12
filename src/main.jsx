23eef46eb9b68743bb07f68bb0148eb8bfc6606c
triudangcoding <quoctrieuzxc@gmailcom>
2024-10-12 12:18:42 +0700

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,35 @@
 
 // import App from '../src/App';
-import React from 'react';
+// import React from 'react';
 import './index.css';
-import ReactDOM from 'react-dom/client';
-import Header from './Header';
-import Form from './Form';
+import { createRoot } from 'react-dom/client'
+// import Number from './App'
+import { Nummber } from './App';
+// import Header from './Header';
+// import Form from './Form';
 
 
-
-function Main() {
-    return (
-        <>  
-        <Header />  
-        <Form />  
-    </>  
-    );
-}
-
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <Main/>
-    </React.StrictMode>
-
+createRoot(document.getElementById("root")).render(
+    <Nummber />
 );
+
+
+// function Main() {
+//     return (
+//         <>  
+//         <Header />  
+//         <Form />  
+//     </>  
+//     );
+// }
+
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//     <React.StrictMode>
+//         <Main/>
+//     </React.StrictMode>
+
+// );
 
 
 
